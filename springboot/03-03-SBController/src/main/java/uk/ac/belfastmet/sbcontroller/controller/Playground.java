9889252759d88dbcbc0d9846f984fae5cf39f786
mcc12068703 <mcc12068703@CLCLRCAC12.belfastmet.int>
2018-11-20 16:10:33 +0000

    - Add controller playground project - Add web pages for each toy story character

--- springboot/03-03-SBController/src/main/java/uk/ac/belfastmet/sbcontroller/controller/Playground.java
+++ springboot/03-03-SBController/src/main/java/uk/ac/belfastmet/sbcontroller/controller/Playground.java
@@ -2,6 +2,7 @@ package uk.ac.belfastmet.sbcontroller.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Playground {
 	
+	@GetMapping("/")
 	public String greeting(Model model) {
-		return "Hello World!";
+		return "<h1>Hello World!</h1>";
+	}
+	
+	@GetMapping("/bingo")
+	public String bingo(Model model) {
+		return "There was a dog in our yard";
+	}
+	
+	@GetMapping("/belfastmet")
+	public String belfastmet(Model model) {
+		return "Leading the city to work";
 	}
 }
