bb1e63f30ed38c5ad1152539cc6f5ba4c0233895
Ryan McCauley (MCC12068703) <MCC12068703@belfastmet.ac.uk>
2018-12-13 11:56:48 +0000

    Add API implementation to get hacker news article. Add ni assembly API project

--- 08-01-hackernews-api/src/main/java/uk/ac/belfastmet/hackernews/controller/NewsController.java
+++ 08-01-hackernews-api/src/main/java/uk/ac/belfastmet/hackernews/controller/NewsController.java
@@ -1,5 +1,26 @@
 package uk.ac.belfastmet.hackernews.controller;
 
-public class NewsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
 
+import uk.ac.belfastmet.hackernews.domain.Article;
+
+@Controller
+public class NewsController {
+	
+	@GetMapping("news/{id}")
+	public String getNews(@PathVariable("id") Integer id ,Model model) {
+		
+		String targetUrl = "https://hacker-news.firebaseio.com/v0/item/" + id + ".json";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		Article article = restTemplate.getForObject(targetUrl, Article.class);
+		
+		model.addAttribute("article", article);
+		
+		return "news";
+	}
 }
